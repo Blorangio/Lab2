7b2d9921bce149a1f1bee1340abf4224a8661cd0
Fenton Pratt <fadpratt@gmail.com>
2023-06-27 10:44:40 -0500

    First Attempt (compiled)

--- src/Lab2.cpp
+++ src/Lab2.cpp
@@ -12,8 +12,12 @@ SYSTEM_THREAD(ENABLED);
 uint16_t value = analogRead(A5);
 void setup() {
   Serial.begin(9600);
+  pinMode(D5, OUTPUT);
 }
 void loop() {
   value = analogRead(A5);
-  Serial.println(value);
+  delay(value);
+  digitalWrite(D5, HIGH);
+  delay(value);
+  digitalWrite(D5, LOW);
 }
